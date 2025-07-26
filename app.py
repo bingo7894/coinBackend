cf467fc8f6f51fbaa6552b06879ed9d9858d7357
bingo7894 <62197197+bingo7894@users.noreply.github.com>
2025-07-26 23:39:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000", # สำหรับ local dev
         "https://coin5-a3x7dsjd7-bingo7894s-projects.vercel.app" # <<--- URL จริงของ Vercel App คุณ
+         "https://coinbackend-pi6n.onrender.com", # <--- เพิ่มบรรทัดนี้
     ],
     allow_methods=["*"],
     allow_headers=["*"],
